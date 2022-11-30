b679164cfbe028072e8fd0d92eb8a48564e2da88
Aineed <ainaspro@gmail.com>
2022-11-30 17:32:52 +0100

    Creating Navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
+        primary: "#00040f", //black (blue charcoal)
+        secondary: "#00f6ff", // cyan
         dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        dimBlue: "rgba(9, 151, 124, 0.1)", // kind of blue / green
       },
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        comfortaa: ["Comfortaa", "cursive"],
       },
     },
     screens: {
